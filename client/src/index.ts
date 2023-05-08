3c921c59dca1d6efbcf4ee2756ba27c5ba30a105
Robin <66969441+CactusHamster@users.noreply.github.com>
2023-05-07 22:17:10 -0700

    Moved HOSTS.txt

--- client/src/index.ts
+++ client/src/index.ts
@@ -1,7 +1,7 @@
 import { exit } from "process";
 import { DiClient } from "./diclient";
 import { readFileSync, existsSync } from "fs";
-let hostfile = "../HOST.txt"
+let hostfile = "./HOST.txt"
 if (!existsSync(hostfile)) {
     console.error("Failed to find HOST.txt file. Please create one in the project directory and write the remote server url to it.");
     exit(1);
